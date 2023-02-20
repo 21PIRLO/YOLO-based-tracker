9abbe8f41a53c6a8c70ff3460d24856db331975a
yixu.cui <yiux.cui@nio.com>
2023-02-20 02:15:36 +0000

    replace zip file url

--- scripts/get_coco.sh
+++ scripts/get_coco.sh
@@ -3,14 +3,18 @@
 # Download command: bash ./scripts/get_coco.sh
 
 # Download/unzip labels
-d='./' # unzip directory
-url=https://github.com/ultralytics/yolov5/releases/download/v1.0/
-f='coco2017labels-segments.zip' # or 'coco2017labels.zip', 68 MB
+# d='./' # unzip directory
+d='/perception/yixu.cui/datas/tracking/coco' # unzip directory
+url=https://github.com/ultralytics/yolov5/releases/download/v1.0/  # 404
+# https://github.com/WongKinYiu/yolov7/releases/download/v0.1/coco2017labels-segments.zip
+url=https://github.com/WongKinYiu/yolov7/releases/download/v0.1/  # available
+f='coco2017labels-segments.zip' # or 'coco2017labels.zip', 177 MB
 echo 'Downloading' $url$f ' ...'
 curl -L $url$f -o $f && unzip -q $f -d $d && rm $f & # download, unzip, remove in background
 
 # Download/unzip images
-d='./coco/images' # unzip directory
+# d='./coco/images' # unzip directory
+d='/perception/yixu.cui/datas/tracking/coco/images' # unzip directory
 url=http://images.cocodataset.org/zips/
 f1='train2017.zip' # 19G, 118k images
 f2='val2017.zip'   # 1G, 5k images
